4447b10892ecec9f343ffa72db6a1c1ed5138a03
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-05-12 10:33:36 -0400

    Completed and working

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,17 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
-
+// import data
+import Data from "../../dummy-data";
 const PostsPage = () => {
   // set up state for your data
+  const [postData, setPostData] = useState(Data);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {postData.map((data) => (
+        <Post post={data} key={data.username} />
+      ))}
     </div>
   );
 };
-
 export default PostsPage;
